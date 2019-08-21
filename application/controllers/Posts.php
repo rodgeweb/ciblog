f246398a98b28aa2bb9abca8b82fe1f7c6106abc
rodge <rodgeweb@gmail.com>
2019-08-21 23:52:48 +0800

    Remove unecessary commented line of code. This is also to test if we can get through pushing without problems.

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -28,8 +28,6 @@ class Posts extends CI_Controller {
             show_404();
         }
 
-        // $data['title'] = $data['post']['title'];
-
 		$this->load->view('templates/header');
 		$this->load->view('posts/view', $data);
 		$this->load->view('templates/footer');
